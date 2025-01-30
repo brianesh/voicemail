cd4a75cb62e6099aba6d0915e8db9935bb5e6859
brianesh <yobrasilva@gmail.com>
2025-01-30 19:55:07 +0300

    first commit

--- popup.js
+++
@@ -1,5 +0,0 @@
-// Listen for a click on the Start Recognition button
-document.getElementById("startRecognitionBtn").addEventListener("click", function() {
-    // Send a message to background.js to start speech recognition
-    chrome.runtime.sendMessage({ action: "startSpeechRecognition" });
-});
