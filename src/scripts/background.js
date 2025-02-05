a3f17014c82a2e39559b1becdfb0535e1a44a4a2
brianesh <yobrasilva@gmail.com>
2025-02-05 12:17:59 +0300

    first commit

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -2,7 +2,7 @@ chrome.runtime.onInstalled.addListener(function () {
     console.log('VOICEMAIL extension installed!');
   });
   
-  // Example listener for events (you can expand functionality here)
+  
   chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     if (message === "getStatus") {
       sendResponse({ status: "Running" });
