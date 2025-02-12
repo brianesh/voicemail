1a4be4ef94ea0325168c948e53bda625de72c265
brianesh <yobrasilva@gmail.com>
2025-02-12 20:06:17 +0300

    first commit

--- src/scripts/content.js
+++
@@ -1,5 +0,0 @@
-console.log("Content script running...");
-
-if (window.location.hostname.includes("mail.google.com") || window.location.hostname.includes("yahoo.com")) {
-  console.log("You are on an email page!");
-}
