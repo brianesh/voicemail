97590bfe5f46446ea3e0c419f89d9ac7c1fe2e5f
brianesh <yobrasilva@gmail.com>
2025-03-10 22:32:23 +0300

    first commit

--- extension/background.js
+++ extension/background.js
@@ -7,10 +7,27 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         console.log("Processing command:", message.command);
 
         let url = null;
+
         if (message.command.includes("open inbox")) {
             url = "https://mail.google.com/mail/u/0/#inbox";
         } else if (message.command.includes("compose email")) {
             url = "https://mail.google.com/mail/u/0/#inbox?compose=new";
+        } else if (message.command.includes("starred")) {
+            url = "https://mail.google.com/mail/u/0/#starred";
+        } else if (message.command.includes("snoozed")) {
+            url = "https://mail.google.com/mail/u/0/#snoozed";
+        } else if (message.command.includes("important")) {
+            url = "https://mail.google.com/mail/u/0/#imp"; // "imp" for important
+        } else if (message.command.includes("sent")) {
+            url = "https://mail.google.com/mail/u/0/#sent";
+        } else if (message.command.includes("drafts")) {
+            url = "https://mail.google.com/mail/u/0/#drafts";
+        } else if (message.command.includes("spam")) {
+            url = "https://mail.google.com/mail/u/0/#spam";
+        } else if (message.command.includes("trash")) {
+            url = "https://mail.google.com/mail/u/0/#trash";
+        } else if (message.command.includes("all mail")) {
+            url = "https://mail.google.com/mail/u/0/#all"; // "all" for all mail
         }
 
         if (url) {
